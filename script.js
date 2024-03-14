ed83f94e2e9d68cb01a0ca43f70b83eabcb2bf76
mariana de veras <mariana.de.veras@eaportal.org>
2024-03-14 09:52:41 -0300

    doze commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const botaoAbrir = document.querySelector(".header-mobile > button");
-const botaoFechar = document.querySelector(".header-mobile nav button");
+const botaoAbrir = document.querySelector("header > button");
+const botaoFechar = document.querySelector("header nav button");
 
 const body = document.querySelector("body");
 const nav = document.querySelector(".header-mobile nav");
